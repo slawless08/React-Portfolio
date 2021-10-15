1071631feff5753053823a317fc9320001bb9076
slawless08 <samuel.l.lawless@gmail.com>
2021-10-15 04:45:51 -0400

    Rendered footer from app.js. Added image links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
             <a href="mailto:samuel.l.lawless@gmail.com" class="footer__link">samuel.l.lawless@gmail.com</a>
             <ul class="socials">
 
-                <li class="socials__item"><a class="socials__link" href="https://www.linkedin.com/in/samuel-lawless-1b5496110/"><img src="./pages/assets/images/linkedIn_24px.png" alt="" /></a></li>
-                <li class="socials__item"><a class="socials__link" href="https://github.com/slawless08"><img src="./pages/assets/images/gitHub_24px.png" alt="" /></a></li>
+                <li class="socials__item"><a class="socials__link" href="https://www.linkedin.com/in/samuel-lawless-1b5496110/"><img src="https://slawless08.github.io/Draft-Portfolio/assets/images/linkedIn_24px.png" alt="" /></a></li>
+                <li class="socials__item"><a class="socials__link" href="https://github.com/slawless08"><img src="https://slawless08.github.io/Draft-Portfolio/assets/images/gitHub_24px.png" alt="" /></a></li>
             </ul>
         </footer>
     )
