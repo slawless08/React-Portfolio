872075f482e4f059f2c80079d415414d629fd6ad
slawless08 <samuel.l.lawless@gmail.com>
2021-12-09 17:15:28 -0500

    Fixed styling issues for the nav bar.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,31 +2,33 @@ import React from 'react';
 
 const Nav = ({currentPage, handlePageChange}) => {
     return (
+        <nav>
         <ul className="nav nav-tabs">
             <li className="nav-item">
                 <a 
                 href="#home" 
                 onClick={()=> handlePageChange('Home')} 
-                className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+                className={currentPage === 'Home' ? 'nav__link active' : 'nav__link'}>
                     Home
                 </a>
             </li>
             <li className="nav-item">
-                <a href="#about-me" onClick={()=> handlePageChange('About-me')} className={currentPage === 'About-me' ? 'nav-link active' : 'nav-link'}>
+                <a href="#about-me" onClick={()=> handlePageChange('About-me')} className={currentPage === 'About-me' ? 'nav__link active' : 'nav__link'}>
                     About Me
                 </a>
             </li>
             <li className="nav-item">
-                <a href="#my-work" onClick={()=> handlePageChange('My-work')} className={currentPage === 'My-work' ? 'nav-link active' : 'nav-link'}>
+                <a href="#my-work" onClick={()=> handlePageChange('My-work')} className={currentPage === 'My-work' ? 'nav__link active' : 'nav__link'}>
                     My Work
                 </a>
             </li>
             <li className="nav-item">
-                <a href="#contact" onClick={()=> handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                <a href="#contact" onClick={()=> handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav__link active' : 'nav__link'}>
                     Contact
                 </a>
             </li>
         </ul>
+        </nav>
     )
 }
 
